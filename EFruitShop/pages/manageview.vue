dde3f489e7be00052a077fe5ff95c1aa1d2f3d14
David Kohalmi <killerkpoke@gmail.com>
2023-05-30 16:01:18 +0200

    Adding css for tr doms

--- EFruitShop/pages/manageview.vue
+++ EFruitShop/pages/manageview.vue
@@ -7,7 +7,10 @@ const data = {
     <div>
         <Header></Header>
         <div class="m-6">
-            <h1 class="text-4xl m-4 font-semibold">Management View</h1>
+            <div class="flex items-center">
+                <h1 class="text-4xl m-4 font-semibold">Management View</h1>
+                <button class="btn btn-success">Add new item</button>
+            </div>
             <div class="overflow-x-auto w-full">
                 <table class="table w-full">
                     <thead>
@@ -22,11 +25,10 @@ const data = {
                             <th>Price</th>
                             <th>Quantity</th>
                             <th>Functions</th>
-                            
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr class="[&>*]:bg-[#1c1f25]">
                             <th>
                                 <label>
                                     <input type="checkbox" class="checkbox" />
@@ -55,7 +57,6 @@ const data = {
                                 <button class="btn btn-error btn-xl text-white my-2">
                                     <NuxtLink to="/deleteItem">
                                         Delete Item
-                                        <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 pr-1" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg> -->
                                     </NuxtLink>
                                 </button></div>
                             </th>
