09f9bea4ee94c9060ee97471212b24df0de25b32
David Kohalmi <killerkpoke@gmail.com>
2023-05-30 14:18:09 +0200

    Add manager view

--- EFruitShop/pages/manageview.vue
+++ EFruitShop/pages/manageview.vue
@@ -6,6 +6,63 @@ const data = {
 <template>
     <div>
         <Header></Header>
-        Management View
+        <div class="m-6">
+            <h1 class="text-4xl m-4 font-semibold">Management View</h1>
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Functions</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div class="mask mask-squircle w-12 h-12">1</div>
+                            </td>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div>
+                                        <div class="font-bold">Banana</div>
+                                    </div>
+                                </div>
+                            </td>
+                                
+                            <td>666</td>
+                            <td>69</td>
+                            <th>
+                                <div class="inline-grid">
+                                <button class="btn btn-warning btn-xl text-white my-2">
+                                    <NuxtLink to="/updateItem">
+                                        Update Item
+                                    </NuxtLink>
+                                </button>
+                                <button class="btn btn-error btn-xl text-white my-2">
+                                    <NuxtLink to="/deleteItem">
+                                        Delete Item
+                                        <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 pr-1" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg> -->
+                                    </NuxtLink>
+                                </button></div>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
