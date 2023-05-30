13abcb40756312b5940cad98d39a3db41efbab53
David Kohalmi <killerkpoke@gmail.com>
2023-05-30 16:53:04 +0200

    adding required validation

--- EFruitShop/pages/addItemview.vue
+++ EFruitShop/pages/addItemview.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { Form, Field, ErrorMessage, defineRule  } from 'vee-validate';
 
+defineRule('required', (value: string | any[]) => {
+  if (!value || !value.length) {
+    return 'This field is required';
+  }
+  return true;
+});
 
 
 
@@ -22,29 +28,29 @@ function onSubmit(values: object) {
                     <label class="label">
                         <span class="label-text">Item name</span>
                     </label>
-                    <Field name="name" type="text" class="input w-full max-w-xs bg-white text-black" ></Field>
-                    <ErrorMessage name="name" />
+                    <Field name="name" type="text" rules="required" class="input w-full max-w-xs bg-white text-black" ></Field>
+                    <ErrorMessage name="name" class="text-red-500" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span class="label-text">Item price</span>
                     </label>
-                    <Field name="price" type="number" class="input w-full max-w-xs bg-white text-black" ></Field>
-                    <ErrorMessage name="price" />
+                    <Field name="price" type="number" rules="required" class="input w-full max-w-xs bg-white text-black" ></Field>
+                    <ErrorMessage name="price" class="text-red-500" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span class="label-text">Item quantity</span>
                     </label>
-                    <Field name="quantity" type="number" class="input w-full max-w-xs bg-white text-black" ></Field>
-                    <ErrorMessage name="quantity" />
+                    <Field name="quantity" type="number" rules="required" class="input w-full max-w-xs bg-white text-black" ></Field>
+                    <ErrorMessage name="quantity" class="text-red-500" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span class="label-text">Item image</span>
                     </label>
-                    <Field name="file" type="file" class="file-input file-input-bordered w-full max-w-xs" ></Field>
-                    <ErrorMessage name="file" />
+                    <Field name="file" type="file" rules="required" class="file-input file-input-bordered w-full max-w-xs" ></Field>
+                    <ErrorMessage name="file" class="text-red-500" />
                 </div>
                 <button class="btn btn-primary mt-6">
                     <!-- <NuxtLink to="/manageview"> -->
