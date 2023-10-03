1a1327900b8c64917fe2a7d494b61d6b1a4ac8ad
David Kohalmi <killerkpoke@gmail.com>
2023-10-03 10:44:11 +0200

    using ls data instead of dummy data

--- EFruitShop/pages/index.vue
+++ EFruitShop/pages/index.vue
@@ -1,86 +1,90 @@
 <script setup lang="ts">
-    const dummydata = [
-        {
-            "id":1,
-            "desc":"ItemDescription",
-            "name":"Big Cute Banana",
-            "quantity":3,
-            "price":400,
-            "tags":[{
-                "id": 1,
-                "name": "Error",
-                "desc": "FATAL ERROR",
-                "context": "error"
-            }]
-        },
-        {
-            "id":2,
-            "desc":"ItemDescription",
-            "name":"Big Cute Banana",
-            "quantity":3,
-            "price":600,
-            "tags":[{
-                "id": 1,
-                "name": "Error",
-                "desc": "FATAL ERROR",
-                "context": "error"
-            }]
-        },
-        {
-            "id":3,
-            "desc":"ItemDescription",
-            "name":"Big Cute Banana",
-            "quantity":3,
-            "price":600,
-            "tags":[{
-                "id": 1,
-                "name": "Error",
-                "desc": "FATAL ERROR",
-                "context": "error"
-            }]
-        },
-        {
-            "id":4,
-            "desc":"ItemDescription",
-            "name":"Big Cute Banana",
-            "quantity":3,
-            "price":600,
-            "tags":[{
-                "id": 1,
-                "name": "Error",
-                "desc": "FATAL ERROR",
-                "context": "error"
-            }]
-        },
-        {
-            "id":5,
-            "desc":"ItemDescription2",
-            "name":"Little Cute Banana",
-            "quantity":2,
-            "price":700,
-            "tags":[
-            {
-                "id": 1,
-                "name": "Error",
-                "desc": "FATAL ERROR",
-                "context": "error"
-            },
-            {
-                "id": 2,
-                "name": "GTK",
-                "desc": "Good to know",
-                "context": "info"
-            },
-            ]
-        },
-    ]
+    // const dummydata = [
+    //     {
+    //         "id":1,
+    //         "desc":"ItemDescription",
+    //         "name":"Big Cute Banana",
+    //         "quantity":3,
+    //         "price":400,
+    //         "tags":[{
+    //             "id": 1,
+    //             "name": "Error",
+    //             "desc": "FATAL ERROR",
+    //             "context": "error"
+    //         }]
+    //     },
+    //     {
+    //         "id":2,
+    //         "desc":"ItemDescription",
+    //         "name":"Big Cute Banana",
+    //         "quantity":3,
+    //         "price":600,
+    //         "tags":[{
+    //             "id": 1,
+    //             "name": "Error",
+    //             "desc": "FATAL ERROR",
+    //             "context": "error"
+    //         }]
+    //     },
+    //     {
+    //         "id":3,
+    //         "desc":"ItemDescription",
+    //         "name":"Big Cute Banana",
+    //         "quantity":3,
+    //         "price":600,
+    //         "tags":[{
+    //             "id": 1,
+    //             "name": "Error",
+    //             "desc": "FATAL ERROR",
+    //             "context": "error"
+    //         }]
+    //     },
+    //     {
+    //         "id":4,
+    //         "desc":"ItemDescription",
+    //         "name":"Big Cute Banana",
+    //         "quantity":3,
+    //         "price":600,
+    //         "tags":[{
+    //             "id": 1,
+    //             "name": "Error",
+    //             "desc": "FATAL ERROR",
+    //             "context": "error"
+    //         }]
+    //     },
+    //     {
+    //         "id":5,
+    //         "desc":"ItemDescription2",
+    //         "name":"Little Cute Banana",
+    //         "quantity":2,
+    //         "price":700,
+    //         "tags":[
+    //         {
+    //             "id": 1,
+    //             "name": "Error",
+    //             "desc": "FATAL ERROR",
+    //             "context": "error"
+    //         },
+    //         {
+    //             "id": 2,
+    //             "name": "GTK",
+    //             "desc": "Good to know",
+    //             "context": "info"
+    //         },
+    //         ]
+    //     },
+    // ]
+
+    const localData = localStorage.getItem('newItem') || "{}";
+    const data = JSON.parse(localData);
+    
 </script>
 <template>
-    <div>
+    <div class="min-h-screen">
         <Header></Header>
         <div class="flex place-content-center">
             <div>
-                <div v-for="dum in dummydata" :key="dum.id">
+                <div v-for="dum in data" :key="dum.id">
                     <Card :dummy="dum" />
                 </div>
             </div>
