db495506373c5b11d9b97135bfa308e0c2c7870d
David Kohalmi <killerkpoke@gmail.com>
2023-10-03 12:17:52 +0200

    handling the img

--- EFruitShop/components/Card.vue
+++ EFruitShop/components/Card.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-    const prop = defineProps(['dummy'])
+    let prop = defineProps(['dummy'])
     let counter = useCounter()
     let price = usePrice()
     let localCounter = ref(0)
-
     let selectedItem = useSelectedItems()
+    let tempImg = new Image();
+    tempImg.src = prop.dummy.image;
+    prop.dummy.tempImage = tempImg.src;
+
     function add() {
-        
         counter.value++
         localCounter.value++
         price.value += prop.dummy.price
@@ -24,8 +26,7 @@
                 if (index > -1) selectedItem.value.splice(index, 1)
             }
         }
-        selectedItem.value.push(item)
-          
+        selectedItem.value.push(item) 
     }
 </script>
 
@@ -33,10 +34,12 @@
     <div class="m-4 rounded-xl text-black 2xl:w-full max-w-2xl w-auto">
         <div class="border-t-4 border-gray-700 border-x-4 rounded-t-xl bg-[#e075b5] p-4 md:flex grid">
             <div class="mx-4">
-                <p class="text-center text-xl font-bold">{{prop.dummy.name}}</p>
+                <p class="text-center text-xl font-bold">{{ prop.dummy.name }}</p>
                 <p class="text-center mt-10">{{ prop.dummy.description }}</p>
             </div>
-            <img class="border-2 border-black rounded-xl w-32 h-32 place-self-center mt-4" src="https://icon-library.com/images/banana-icon/banana-icon-5.jpg"/>
+            <img class="border-2 border-black rounded-xl w-32 h-32 place-self-center mt-4" 
+            :src="prop.dummy.tempImage"
+            />
         </div>
         <div class="border-b-4 border-gray-700 border-x-4 rounded-b-xl bg-[#f3a5d3] p-4 grid">
         <div> 
@@ -45,7 +48,7 @@
             </div>
             <div class="mt-2 font-bold">
                 <label>Price:</label>
-                <label>${{prop.dummy.price}}</label>
+                <label>${{ prop.dummy.price }}</label>
             </div>
         </div>
         <button class="btn btn-primary mt-4 place-self-center capitalize" @click=add>
